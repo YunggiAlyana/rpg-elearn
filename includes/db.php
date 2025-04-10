2776e0dc1422720c74a4d3adfcc7b58f7ca37e5a
Yunggi Alyana <yunggialyana123@gmail.com>
2025-04-10 20:25:05 +0700

    Fix PDO connection variable from $db to $pdo

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=rpg_elearn", "root", "");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=localhost;dbname=rpg_elearn", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
